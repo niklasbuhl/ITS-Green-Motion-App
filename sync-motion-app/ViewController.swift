b73cf501d408701dfb47c0ebd78610d260987372
nb <hello@niklasbuhl.xyz>
2019-05-13 14:40:56 +0200

    First commit

---
+++ sync-motion-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sync-motion-app
+//
+//  Created by Niklas Buhl on 13/05/2019.
+//  Copyright © 2019 Niklas Buhl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
